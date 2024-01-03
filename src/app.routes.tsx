ba5d87726590d4e7d20be266aa08a283c9734b51
Doroshkeviches <Doroshkeviches@yandex.ru>
2024-01-03 17:40:06 +0300

    feat: delete orders_2

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -29,7 +29,6 @@ const PublicRoute: FC<{ element: any }> = ({ element: Element }) => (
 
 // ======= pages ======= //
 const AuthRoutes = React.lazy(() => import("./app/auth/index"))
-const OrdersRoutes = React.lazy(() => import("./app/orders/index"))
 
 
 
@@ -37,7 +36,6 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/admin/auth/*' element={<PublicRoute element={AuthRoutes} />} />
-      <Route path='/admin/orders/*' element={<PublicRoute element={OrdersRoutes} />} />
       <Route path='/*' element={<PublicRoute element={() => <div>NO SUCH ROUTE</div>} />} />
     </Routes>
   );
