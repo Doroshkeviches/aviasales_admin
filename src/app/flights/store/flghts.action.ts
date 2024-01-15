bd6d53366db1e177c5d75219031f063d61cc9ec3
sevichok <90391121+sevichok@users.noreply.github.com>
2024-01-15 17:48:45 +0300

    sortedBy select

--- src/app/flights/store/flghts.action.ts
+++ src/app/flights/store/flghts.action.ts
@@ -1,5 +1,4 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { Flight } from "../types/Flight.type";
 import repository from "src/repository";
 import { City } from "../types/City.type";
 import { Paths } from "../types/Paths.type";
@@ -7,7 +6,7 @@ import { GetFlights } from "../types/GetFlights.type";
 
 export const getFlights = createAsyncThunk<Paths[], GetFlights>("GET/flights", async (body, { rejectWithValue }) => {
     try {
-        const response = await repository.get(`/flights?from_city=${body.from_city}&to_city=${body.to_city}&date=${body.start_flight_date}&isReturn=${body.isReturn}&returnDate=${body.return_flight_date}`);
+        const response = await repository.get(`/flights?sortedBy=${body.sortedBy}&from_city=${body.from_city}&to_city=${body.to_city}&date=${body.start_flight_date}&isReturn=${body.isReturn}&returnDate=${body.return_flight_date}`);
         return response.data
     } catch (error: any) {
         return rejectWithValue(error);
